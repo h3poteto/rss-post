234c8fa70546b990dfb3e0eb518907a2d6c46d35
AkiraFukushima <h3.poteto@gmail.com>
2024-05-07 18:42:26 +0900

    Skip update redis if failed to post

--- hatena.rb
+++ hatena.rb
@@ -21,7 +21,7 @@ URI.open(url) do |rss|
 
   last_updated_date = Time.parse(last_updated)
 
-  feed.items.each do |item|
+  feed.items.reverse_each do |item|
     next if item.content_encoded.length < 1
     next if item.dc_date <= last_updated_date
     body = "#{item.content_encoded} / #{item.title} - #{item.link}"
@@ -30,10 +30,11 @@ URI.open(url) do |rss|
       params = { access_token: ENV['ACCESS_TOKEN'], status: body, visibility: "public" }
       response = Net::HTTP.post_form(uri, params)
       puts "Posted: #{response.code} #{response.body}"
+      next if response.code != "200" || response.code != "201"
+      redis.set('last_updated', item.dc_date)
     else
       puts "Dry-run: #{body}"
+      redis.set('last_updated', item.dc_date)
     end
   end
-
-  redis.set('last_updated', feed.items[0].dc_date)
 end
